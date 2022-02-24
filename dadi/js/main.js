830053e91b28e41018533d031757fbd61f543ac9
8devil3 <8dev.il3@gmail.com>
2022-02-24 16:18:52 +0100

    aggiunto HTML ai dadi

--- dadi/js/main.js
+++ dadi/js/main.js
@@ -1,16 +1,42 @@
-let numberPlayer1 = Math.floor(Math.random() * 6) + 1;
-let numberPlayer2 = Math.floor(Math.random() * 6) + 1;
+let player1DiceNumber = '';
+let player2DiceNumber = '';
+
+// variabili HTML
+const player1DiceNumberHTML = document.querySelector('#player1');
+const player2DiceNumberHTML = document.querySelector('#player2');
+const winnerHTML = document.querySelector('#winner');
+const btnThrowingDice = document.querySelector('#dice-throwing');
+
+// messaggi
+const player1WinMsg = 'Player 1 win!';
+const player2WinMsg = 'Player 2 win!';
+const drawMsg = 'Draw!';
 
 
-if (numberPlayer1 == numberPlayer2) {
 
-    console.log('Draw!');
+// HTML
+btnThrowingDice.addEventListener('click', diceThrowing);
 
-} else if (numberPlayer1 > numberPlayer2) {
 
-    console.log('Player 1 win!');
+// function
+function diceThrowing() {
+    
+    player1DiceNumber = Math.floor(Math.random() * 6) + 1;
+    player2DiceNumber = Math.floor(Math.random() * 6) + 1;
+    
+    player1DiceNumberHTML.innerHTML = player1DiceNumber;
+    player2DiceNumberHTML.innerHTML = player2DiceNumber;
 
-} else {
+    if (player1DiceNumber == player2DiceNumber) {
 
-    console.log('Player 2 win!');
+        winnerHTML.innerHTML = drawMsg;
+
+    } else if (player1DiceNumber > player2DiceNumber) {
+
+        winnerHTML.innerHTML = player1WinMsg;
+
+    } else {
+
+        winnerHTML.innerHTML = player2WinMsg;
+    }
 }
