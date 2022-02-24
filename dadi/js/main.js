66779f80fde1b14db855a31d18f7402202d8ea9a
8devil3 <8dev.il3@gmail.com>
2022-02-24 17:34:12 +0100

    aggiunto stile ai dadi

--- dadi/js/main.js
+++ dadi/js/main.js
@@ -2,7 +2,7 @@
 const player1DiceNumberHTML = document.querySelector('#player1');
 const player2DiceNumberHTML = document.querySelector('#player2');
 const winnerHTML = document.querySelector('#winner');
-const btnThrowingDice = document.querySelector('#dice-throwing');
+const btnRollDice = document.querySelector('#dice-roll');
 
 // messaggi
 const player1WinMsg = 'Player 1 win!';
@@ -12,28 +12,39 @@ const drawMsg = 'Draw!';
 
 
 // HTML
-btnThrowingDice.addEventListener('click', diceThrowing);
+btnRollDice.addEventListener('click', diceRoll);
 
 
 // function
-function diceThrowing() {
-    
+function diceRoll() {
+
     let player1DiceNumber = Math.floor(Math.random() * 6) + 1;
     let player2DiceNumber = Math.floor(Math.random() * 6) + 1;
-    
+
     player1DiceNumberHTML.innerHTML = 'Player 1 number is: ' + player1DiceNumber;
     player2DiceNumberHTML.innerHTML = 'Player 2 number is: ' + player2DiceNumber;
+
+    player1DiceNumberHTML.classList.remove('win', 'lose', 'draw');
+    player2DiceNumberHTML.classList.remove('win', 'lose', 'draw');
 
     if (player1DiceNumber == player2DiceNumber) {
 
         winnerHTML.innerHTML = drawMsg;
+        player1DiceNumberHTML.classList.add('draw');
+        player2DiceNumberHTML.classList.add('draw');
 
     } else if (player1DiceNumber > player2DiceNumber) {
 
         winnerHTML.innerHTML = player1WinMsg;
+        player1DiceNumberHTML.classList.add('win');
+        player2DiceNumberHTML.classList.add('lose');
 
     } else {
 
         winnerHTML.innerHTML = player2WinMsg;
+        player1DiceNumberHTML.classList.add('lose');
+        player2DiceNumberHTML.classList.add('win');
     }
+
+
 }
