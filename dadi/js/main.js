3b6310842af29c9971de649e7eeb3d7f58796c2e
8devil3 <8dev.il3@gmail.com>
2022-02-24 14:18:51 +0100

    dadi in console

---
+++ dadi/js/main.js
@@ -0,0 +1,16 @@
+let numberPlayer1 = Math.floor(Math.random() * 6) + 1;
+let numberPlayer2 = Math.floor(Math.random() * 6) + 1;
+
+
+if (numberPlayer1 == numberPlayer2) {
+
+    console.log('Draw!');
+
+} else if (numberPlayer1 < numberPlayer2) {
+
+    console.log('Player 2 win!');
+
+} else {
+
+    console.log('Player 1 win!');
+}
